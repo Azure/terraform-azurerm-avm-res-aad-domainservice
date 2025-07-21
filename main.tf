109227c79cb7da76c189a951b44a3c8e8fa47061
Itamar Hirosh <itamarh@microsoft.com>
2025-07-21 11:04:54 +0300

    NSG RULES

--- main.tf
+++ main.tf
@@ -1,8 +1,92 @@
-# TODO: Replace this dummy resource azurerm_resource_group.TODO with your module resource
-resource "azurerm_resource_group" "TODO" {
-  location = var.location
-  name     = var.name # calling code must supply the name
-  tags     = var.tags
+
+resource "azurerm_resource_provider_registration" "this" {
+  name = "Microsoft.AAD"
+}
+
+
+locals {
+  nsg_resource_group_name = split(var.nsg_rules.nsg_resource_id, "/")[4]
+  nsg_name                = split(var.nsg_rules.nsg_resource_id, "/")[8]
+}
+
+#TODO: consider adding NSG with rules for domain services
+
+
+resource "azurerm_network_security_rule" "rdp" {
+  count = var.nsg_rules.allow_rdp_access ? 1 : 0
+  name                        = "AllowRD"
+  network_security_group_name = local.nsg_name
+  resource_group_name         = local.nsg_resource_group_name
+  priority                    = var.nsg_rules.rdp_rule_priority
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_ranges     = ["3389"]
+  source_address_prefix       = "CorpNetSaw"
+  destination_address_prefix  = "*"
+}
+
+resource "azurerm_network_security_rule" "winrm" {
+  name                        = "AllowPSRemoting"
+  network_security_group_name = local.nsg_name
+  resource_group_name         = local.nsg_resource_group_name
+  priority                    = var.nsg_rules.winrm_rule_priority
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_ranges     = ["5986"]
+  source_address_prefix       = "AzureActiveDirectoryDomainServices"
+  destination_address_prefix  = "*"
+  
+}
+
+resource "azurerm_active_directory_domain_service" "this" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  domain_name           = var.domain_name
+  sku                   = var.sku
+  domain_configuration_type = var.domain_configuration_type
+  filtered_sync_enabled = var.filtered_sync_enabled
+  
+  dynamic "secure_ldap" {
+    for_each = var.secure_ldap.enabled == true ? [1] : []
+    content {
+      enabled = var.secure_ldap.enabled
+      pfx_certificate = var.secure_ldap.pfx_certificate
+      pfx_certificate_password = var.secure_ldap.pfx_certificate_password
+    }
+  }
+
+  dynamic "notifications" {
+    for_each = var.notifications != null ? var.notifications : {}
+    content {
+      notify_dc_admins      = var.notifications.notify_dc_admins
+      notify_global_admins  = var.notifications.notify_global_admins
+    }
+  }
+
+  initial_replica_set {
+    subnet_id = var.subnet_resource_id
+  }
+
+  notifications {
+    notify_dc_admins      = true
+    notify_global_admins  = true
+  }
+
+  security {
+    sync_kerberos_passwords = true
+    sync_ntlm_passwords     = true
+    sync_on_prem_passwords  = true
+  }
+  # This will change to "fully synced" once the service is fully deployed. and will enforce replacement 
+  
+  lifecycle {
+    ignore_changes = [ domain_configuration_type, tags ]
+  }
 }
 
 # required AVM resources interfaces
@@ -11,7 +95,7 @@ resource "azurerm_management_lock" "this" {
 
   lock_level = var.lock.kind
   name       = coalesce(var.lock.name, "lock-${var.lock.kind}")
-  scope      = azurerm_resource_group.TODO.id # TODO: Replace with your azurerm resource name
+  scope      = azurerm_active_directory_domain_service.this.id
   notes      = var.lock.kind == "CanNotDelete" ? "Cannot delete the resource or its child resources." : "Cannot delete or modify the resource or its child resources."
 }
 
@@ -19,7 +103,7 @@ resource "azurerm_role_assignment" "this" {
   for_each = var.role_assignments
 
   principal_id                           = each.value.principal_id
-  scope                                  = azurerm_resource_group.TODO.id # TODO: Replace this dummy resource azurerm_resource_group.TODO with your module resource
+  scope                                  = azurerm_active_directory_domain_service.this.id
   condition                              = each.value.condition
   condition_version                      = each.value.condition_version
   delegated_managed_identity_resource_id = each.value.delegated_managed_identity_resource_id
--- variables.tf
+++ variables.tf
@@ -4,16 +4,76 @@ variable "location" {
   nullable    = false
 }
 
+variable "subnet_resource_id" {
+  type        = string
+  description = <<DESCRIPTION
+The ID of the subnet to deploy the MEDS insatnce into.
+DESCRIPTION
+}
+
+variable "domain_name" {
+  type        = string
+  #TODO - validate domain name
+  description = <<DESCRIPTION
+The domain name to use for the Entra ID Domain Services instance.
+DESCRIPTION
+}
+
+#TODO - validate meaning of this setting
+variable "domain_configuration_type" {
+  type        = string
+  description = <<DESCRIPTION
+The domain configuration type to use for the Entra ID Domain Services instance. Possible values are `FullySynced` and `ResourceTrusting`.
+DESCRIPTION
+}
+
+variable "nsg_rules" {
+  type        = object({
+    nsg_resource_id = string
+    allow_rdp_access = optional(bool, false)
+    rdp_rule_priority = number
+    winrm_rule_priority = number
+  })
+  description = "an object containing the priorities for the NSG rules to be created. The following properties can be specified: `rdp_rule_priority` and `winrm_rule_priority`."
+}
+
+variable "filtered_sync_enabled" {
+  type        = bool
+  description = <<DESCRIPTION
+Whether or not to enable filtered sync for the Entra ID Domain Services instance.
+DESCRIPTION
+  
+}
+
+variable "secure_ldap" {
+  type        = object({
+    enabled = optional(bool, false)
+    external_access_enabled = optional(bool, false)
+    pfx_certificate = string
+    pfx_certificate_password = string
+  })
+  sensitive = true
+  default = null
+  description = <<DESCRIPTION
+A map describing the secure LDAP settings to use for the Entra ID Domain Services instance. This includes the following properties:
+- `enabled` - (Optional) Whether or not to enable secure LDAP. Defaults to `false`.
+- `external_access_enabled` - (Optional) Whether or not to enable external access for secure LDAP. Defaults to `false`.
+- `pfx_certificate` - (Required) The PFX certificate to use for secure LDAP.
+- `pfx_certificate_password` - (Required) The password for the PFX certificate.
+DESCRIPTION
+}
+
+variable "sku" {
+  type        = string
+  description = <<DESCRIPTION
+The SKU to use for the Entra ID Domain Services instance. Possible values are `Standard`, `Enterprise` and `Premium`.
+DESCRIPTION
+  
+}
+
 variable "name" {
   type        = string
   description = "The name of the this resource."
-
-  validation {
-    condition     = can(regex("TODO", var.name))
-    error_message = "The name must be TODO." # TODO remove the example below once complete:
-    #condition     = can(regex("^[a-z0-9]{5,50}$", var.name))
-    #error_message = "The name must be between 5 and 50 characters long and can only contain lowercase letters and numbers."
-  }
 }
 
 # This is required for most resource modules
@@ -136,70 +196,6 @@ DESCRIPTION
   nullable    = false
 }
 
-variable "private_endpoints" {
-  type = map(object({
-    name = optional(string, null)
-    role_assignments = optional(map(object({
-      role_definition_id_or_name             = string
-      principal_id                           = string
-      description                            = optional(string, null)
-      skip_service_principal_aad_check       = optional(bool, false)
-      condition                              = optional(string, null)
-      condition_version                      = optional(string, null)
-      delegated_managed_identity_resource_id = optional(string, null)
-    })), {})
-    lock = optional(object({
-      kind = string
-      name = optional(string, null)
-    }), null)
-    tags                                    = optional(map(string), null)
-    subnet_resource_id                      = string
-    private_dns_zone_group_name             = optional(string, "default")
-    private_dns_zone_resource_ids           = optional(set(string), [])
-    application_security_group_associations = optional(map(string), {})
-    private_service_connection_name         = optional(string, null)
-    network_interface_name                  = optional(string, null)
-    location                                = optional(string, null)
-    resource_group_name                     = optional(string, null)
-    ip_configurations = optional(map(object({
-      name               = string
-      private_ip_address = string
-    })), {})
-  }))
-  default     = {}
-  description = <<DESCRIPTION
-A map of private endpoints to create on this resource. The map key is deliberately arbitrary to avoid issues where map keys maybe unknown at plan time.
-
-- `name` - (Optional) The name of the private endpoint. One will be generated if not set.
-- `role_assignments` - (Optional) A map of role assignments to create on the private endpoint. The map key is deliberately arbitrary to avoid issues where map keys maybe unknown at plan time. See `var.role_assignments` for more information.
-- `lock` - (Optional) The lock level to apply to the private endpoint. Default is `None`. Possible values are `None`, `CanNotDelete`, and `ReadOnly`.
-- `tags` - (Optional) A mapping of tags to assign to the private endpoint.
-- `subnet_resource_id` - The resource ID of the subnet to deploy the private endpoint in.
-- `private_dns_zone_group_name` - (Optional) The name of the private DNS zone group. One will be generated if not set.
-- `private_dns_zone_resource_ids` - (Optional) A set of resource IDs of private DNS zones to associate with the private endpoint. If not set, no zone groups will be created and the private endpoint will not be associated with any private DNS zones. DNS records must be managed external to this module.
-- `application_security_group_resource_ids` - (Optional) A map of resource IDs of application security groups to associate with the private endpoint. The map key is deliberately arbitrary to avoid issues where map keys maybe unknown at plan time.
-- `private_service_connection_name` - (Optional) The name of the private service connection. One will be generated if not set.
-- `network_interface_name` - (Optional) The name of the network interface. One will be generated if not set.
-- `location` - (Optional) The Azure location where the resources will be deployed. Defaults to the location of the resource group.
-- `resource_group_name` - (Optional) The resource group where the resources will be deployed. Defaults to the resource group of this resource.
-- `ip_configurations` - (Optional) A map of IP configurations to create on the private endpoint. If not specified the platform will create one. The map key is deliberately arbitrary to avoid issues where map keys maybe unknown at plan time.
-  - `name` - The name of the IP configuration.
-  - `private_ip_address` - The private IP address of the IP configuration.
-DESCRIPTION
-  nullable    = false
-}
-
-# This variable is used to determine if the private_dns_zone_group block should be included,
-# or if it is to be managed externally, e.g. using Azure Policy.
-# https://github.com/Azure/terraform-azurerm-avm-res-keyvault-vault/issues/32
-# Alternatively you can use AzAPI, which does not have this issue.
-variable "private_endpoints_manage_dns_zone_group" {
-  type        = bool
-  default     = true
-  description = "Whether to manage private DNS zone groups with this module. If set to false, you must manage private DNS zone groups externally, e.g. using Azure Policy."
-  nullable    = false
-}
-
 variable "role_assignments" {
   type = map(object({
     role_definition_id_or_name             = string
@@ -231,4 +227,18 @@ variable "tags" {
   type        = map(string)
   default     = null
   description = "(Optional) Tags of the resource."
+}
+
+
+variable "notifications" {
+  type = object({
+    notify_dc_admins      = optional(bool, true)
+    notify_global_admins  = optional(bool, true)
+  })
+  default     = null
+  description = <<DESCRIPTION
+A map describing the notifications settings to use for the Entra ID Domain Services instance. This includes the following properties:
+- `notify_dc_admins` - (Optional) Whether or not to notify domain controllers administrators. Defaults to `true`.
+- `notify_global_admins` - (Optional) Whether or not to notify global administrators. Defaults to `true`.
+DESCRIPTION
 }
